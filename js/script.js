c4b49c6dbe9c9b88c5ea2870a2f24190e1c110a1
Daniel228337 <danielatahanov053@gmail.com>
2024-07-24 15:10:52 +0500

    Добавил в игнорДобавил в игнор

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// alert('hello');
+
+/*const result = confirm("Are you here?");
+console.log(result);*/
+
+const answer = prompt("Сколько вам лет?","");
+if answer == 18
+    else die;
+console.log(answer);
